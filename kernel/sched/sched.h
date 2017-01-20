fc9e56cbec8f2a0de62db012e22b57dedfb8ac2f
Joonwoo Park <joonwoop@codeaurora.org>
2017-01-20 11:10:15 -0800

    sched: WALT: fix potential overflow
    Task demand and CPU util are in u64.
    
    Change-Id: If7ec1623e723026d3346201122aab0303a6d2ba2
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    (cherry picked from commit c8b8c92bbc8954ae18cba7eb0c1f3a06a244129c)
    [trivial cherry-pick issues]
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    
    Conflicts:
    	kernel/sched/sched.h

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1764,8 +1764,8 @@ static inline unsigned long __cpu_util(int cpu, int delta)
 
 #ifdef CONFIG_SCHED_WALT
 	if (!walt_disabled && sysctl_sched_use_walt_cpu_util) {
-		util = cpu_rq(cpu)->cumulative_runnable_avg << SCHED_CAPACITY_SHIFT;
-		util = div_u64(util, walt_ravg_window);
+		util = div64_u64(cpu_rq(cpu)->cumulative_runnable_avg,
+			       walt_ravg_window >> SCHED_CAPACITY_SHIFT);
 		goto util_walt;
 	}
 #endif
@@ -1795,8 +1795,8 @@ static inline unsigned long cpu_util_freq(int cpu)
 
 #ifdef CONFIG_SCHED_WALT
 	if (!walt_disabled && sysctl_sched_use_walt_cpu_util) {
-		util = cpu_rq(cpu)->prev_runnable_sum << SCHED_CAPACITY_SHIFT;
-		do_div(util, walt_ravg_window);
+		util = div64_u64(cpu_rq(cpu)->prev_runnable_sum,
+				 walt_ravg_window >> SCHED_CAPACITY_SHIFT);
 		goto util_walt;
 	}
 #endif
