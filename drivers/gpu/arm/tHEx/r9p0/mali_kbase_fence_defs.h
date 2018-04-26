bd0ff66572ff6f3ee86ac3f177f4a885cbaa3cda
FAROVITUS <farovitus@gmail.com>
2018-04-26 17:29:30 +0300

    gpu:r9p0: adaptation to 'dma-buf' upstream changes
    Note:
    - Upstreaming 'dma-buf' seems to cause some issues when compiling with our gpu driver, so let's adapt our gpu driver (r9p0) to those changes.
    
    Signed-off-by: FAROVITUS <farovitus@gmail.com>

--- drivers/gpu/arm/tHEx/r9p0/mali_kbase_fence_defs.h
+++ drivers/gpu/arm/tHEx/r9p0/mali_kbase_fence_defs.h
@@ -46,7 +46,7 @@
 #define dma_fence_is_signaled(a) fence_is_signaled(a)
 #define dma_fence_add_callback(a, b, c) fence_add_callback(a, b, c)
 #define dma_fence_remove_callback(a, b) fence_remove_callback(a, b)
-#define dma_fence_get_status(a) (fence_is_signaled(a) ? (a)->status ?: 1 : 0)
+#define dma_fence_get_status(a) (fence_get_status(a))
 
 #else
 
