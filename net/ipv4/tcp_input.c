40023202cf11c29b370042c6be2fb565da2b6fa8
FAROVITUS <farovitus@gmail.com>
2020-03-28 02:14:41 +0200

    net: ipv4: tcp_input: protect MPTCP and no-MPTCP code with a condition
    * This change fix the previous error we had when we upstreamed
      the source.

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -571,14 +571,16 @@ static int __tcp_grow_window(const struct sock *sk, const struct sk_buff *skb)
 static void tcp_grow_window(struct sock *sk, const struct sk_buff *skb)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
-	int room;
 
-	room = min_t(int, tp->window_clamp, tcp_space(sk)) - tp->rcv_ssthresh;
 #ifdef CONFIG_MPTCP
 	struct sock *meta_sk = mptcp(tp) ? mptcp_meta_sk(sk) : sk;
 	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
 	if (is_meta_sk(sk))
 		return;
+#else
+int room;
+
+room = min_t(int, tp->window_clamp, tcp_space(sk)) - tp->rcv_ssthresh;
 #endif
 
 	/* Check #1 */
